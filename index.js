863f07b6303207604256ddd86ddc8a50397f0d19
Juan V <jvillanelo@gmail.com>
2020-05-25 21:05:10 -0400

    Change displayed text to verify update on openshift

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Updated after making the repository private'))
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
 	ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
