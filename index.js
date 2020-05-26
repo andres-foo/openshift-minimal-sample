5d197c219a76ed6caeae7628620ea3de13e03d71
Juan V <jvillanelo@gmail.com>
2020-05-25 20:56:19 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+	ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
+	
+app.listen(port, ip)
+console.log('Server running on http://%s:%s', ip, port);
