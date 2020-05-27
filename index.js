839c09981d22ed4a0bc950d8d7a25d3ac63af23e
andres-foo <37823032+andres-foo@users.noreply.github.com>
2020-05-27 09:10:06 -0400

    Change default IP to 0.0.0.0

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 app.get('/', (req, res) => res.send('Updated after making the repository private'))
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-	ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+	ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
 	
 app.listen(port, ip)
 console.log('Server running on http://%s:%s', ip, port);
