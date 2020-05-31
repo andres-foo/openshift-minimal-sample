c09eda863a3e3fcb49bcebe504d505107c7efc31
andres-foo <37823032+andres-foo@users.noreply.github.com>
2020-05-30 23:02:57 -0400

    Change displayed text when app is run

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Updated after making the repository private'))
+app.get('/', (req, res) => res.send('Hello from openshift'))
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
 	ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
